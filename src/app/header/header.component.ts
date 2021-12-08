379c322e30b98ebb9ef1fc07c27286721f303d1e
GioMelik11 <62147377+GioMelik11@users.noreply.github.com>
2021-12-09 00:02:03 +0400

    header menu dropdown area added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('dropdownChange', { static: true }) dropdownChange: ElementRef<HTMLElement>;
+  checkdropdown: any;
 
-  constructor() { }
+  constructor(dropdownChange: ElementRef<HTMLElement>) {
+    this.dropdownChange = dropdownChange;
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  dropdownMenu() {
+    this.checkdropdown = this.dropdownChange.nativeElement.getAttribute("aria-dropdown");
+
+    if (this.checkdropdown == "false") {
+      this.dropdownChange.nativeElement.setAttribute("aria-dropdown", "true");
+      this.dropdownChange.nativeElement.style.transform = "rotate(180deg)";
+    } else {
+      this.dropdownChange.nativeElement.setAttribute("aria-dropdown", "false");
+      this.dropdownChange.nativeElement.style.transform = "rotate(0deg)"
+    }
   }
 
 }
