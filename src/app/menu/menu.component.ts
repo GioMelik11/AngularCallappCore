318ca58e2743fb542b8d4324fc6380cac6db98f8
Gio Melikishvili <csmevar5@gmail.com>
2021-12-09 13:14:52 +0400

    addded menu items

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,12 +7,30 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   @ViewChild('menu', { static: true }) menu: ElementRef<HTMLDivElement>;
+  menuItem: any = new Object();
 
   constructor(menu: ElementRef<HTMLDivElement>) {
     this.menu = menu;
   }
 
   ngOnInit(): void {
+    this.menuItem = [{
+      id: "1",
+      name: "მთავარი",
+      route: "dashboard"
+    }, {
+      id: "1",
+      name: "შემომავალი",
+      route: "incomming"
+    }, {
+      id: "1",
+      name: "ტესტ გვერდი",
+      route: "test1"
+    }, {
+      id: "1",
+      name: "ტესტ გვერდი 2",
+      route: "test2"
+    }];
   }
 
 }
