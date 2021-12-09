48ef230a6c7226fe1cc611fa8bb89bf18c76b0b6
Gio Melikishvili <csmevar5@gmail.com>
2021-12-09 14:35:06 +0400

    added current page active

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,19 +1,27 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
   @ViewChild('menu', { static: true }) menu: ElementRef<HTMLDivElement>;
   menuItem: any = new Object();
+  currentRoute: any;
 
-  constructor(menu: ElementRef<HTMLDivElement>) {
+  constructor(menu: ElementRef<HTMLDivElement>, private router: Router) {
     this.menu = menu;
-  }
 
-  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.currentRoute = event as Object;
+        this.currentRoute = this.currentRoute.url;
+      })
+
     this.menuItem = [{
       id: "1",
       name: "მთავარი",
