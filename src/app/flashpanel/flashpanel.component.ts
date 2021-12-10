8a80956d40b9d99275894839d03f2b28bc7c32d7
GioMelik11 <62147377+GioMelik11@users.noreply.github.com>
2021-12-10 22:45:44 +0400

    changes flashoanel structure

--- src/app/flashpanel/flashpanel.component.ts
+++ src/app/flashpanel/flashpanel.component.ts
@@ -9,6 +9,7 @@ import { ModalComponent } from './modal/modal.component';
 export class FlashpanelComponent implements OnInit {
   @ViewChild(ModalComponent, { read: ElementRef }) private footerElementRef: ElementRef;
   panelItems: any = new Object();
+  clickedItem: any;
 
   constructor(private modalToggle: ElementRef) {
     this.footerElementRef = modalToggle;
@@ -40,19 +41,27 @@ export class FlashpanelComponent implements OnInit {
     return data;
   }
 
-  showHidePanel(source: number) {
+  showHidePanel(source: number, targetId: string) {
     var element = this.footerElementRef.nativeElement.children[0];
+    this.clickedItem = document.getElementById(targetId);
     this.dynamicGetdialogContent(element, source);
 
   }
 
   dynamicGetdialogContent(element: HTMLElement, source: number) {
-    switch (element.getAttribute("aria-hidden")) {
-      case "true":
-        this.showDialog(element);
+
+    switch (this.clickedItem.getAttribute("aria-show")) {
+      case "false":
+        this.hideDialog(element);
+        this.clickedItem.setAttribute("aria-show", 'true');
+        this.hideAll(document.querySelectorAll("div[flash-panel-item]"), this.clickedItem);
+        setTimeout(() => {
+          this.showDialog(element);
+        }, 100);
         this.generateContent(element.children[0].children[0]);
         break;
-      case "false":
+      case "true":
+        this.clickedItem.setAttribute("aria-show", 'false');
         this.hideDialog(element);
         break;
       default:
@@ -67,7 +76,7 @@ export class FlashpanelComponent implements OnInit {
     element.setAttribute("style", "display: block;");
   }
 
-  hideDialog(element: HTMLElement) {
+  hideDialog(element: any) {
     element.setAttribute("class", "modal");
     element.setAttribute("aria-hidden", "true");
     element.setAttribute("style", "display: none;");
@@ -75,6 +84,15 @@ export class FlashpanelComponent implements OnInit {
 
   generateContent(element: any) {
     element.innerHTML = "No Data";
+  }
+
+  hideAll(elements: NodeList, el: any) {
+    elements.forEach(element => {
+      var html = element as any;
+      if (el.getAttribute("id") != html.getAttribute("id")) {
+        html.setAttribute("aria-show", "false");
+      }
+    });
   }
 
 }
