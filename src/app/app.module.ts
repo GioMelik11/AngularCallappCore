145957abb50b24920ac22fe37a9ba7cb2bd989f2
GioMelik11 <62147377+GioMelik11@users.noreply.github.com>
2021-12-10 21:08:16 +0400

    Revert "added ngx-boostrap"
    This reverts commit 9838912fcf89938168714061b65432af04209a89.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthComponent } from './auth/auth.component';
 import { FlashpanelComponent } from './flashpanel/flashpanel.component';
 import { ModalComponent } from './modal/modal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { AlertModule, AlertConfig } from 'ngx-bootstrap/alert';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { FormsModule } from '@angular/forms';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
-import { BsDropdownModule, BsDropdownConfig } from 'ngx-bootstrap/dropdown';
-import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -38,19 +28,9 @@ import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    BrowserAnimationsModule,
-    AccordionModule,
-    AlertModule,
-    ButtonsModule,
-    FormsModule,
-    CarouselModule,
-    CollapseModule,
-    BsDatepickerModule.forRoot(),
-    BsDropdownModule,
-    ModalModule
+    FontAwesomeModule
   ],
-  providers: [AlertConfig, BsDatepickerConfig, BsDropdownConfig, BsModalService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
